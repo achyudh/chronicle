1bf419950b2bfefead8b7cdb7e32dbdb739cb8e8
Achyudh Ram <achyudhk@gmail.com>
2018-11-11 07:13:36 -0500

    Add tokenize_top_n to preprocessing utils

--- lib/util/preprocessing.py
+++ lib/util/preprocessing.py
@@ -5,6 +5,17 @@ from nltk.corpus import stopwords
 english_stopwords = stopwords.words("english")
 
 
+def tokenize_top_n(text, top_n=2, stemming=True):
+    """
+    Tokenizes only the top_n lines for the given text
+    """
+    split_string = text.split('.')
+    top_n_string = ""
+    for string in range(min(len(split_string), top_n)):
+        top_n_string += (split_string[0].strip() + ". ")
+    return tokenize(top_n_string, stemming)
+
+
 def tokenize(text, stemming=True):
     words = [word.lower() for word in word_tokenize(text) if word.lower() not in english_stopwords]
     if stemming:
