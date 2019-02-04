2cd61bb0ae93f60e1de2d7f983d330b4c1f98bd9
Achyudh Ram <achyudhk@gmail.com>
2019-02-04 03:26:28 -0500

    Add support for TREC datasets

--- lib/data/args.py
+++ lib/data/args.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 def get_args():
     parser = ArgumentParser(description="Chronicle - lib/data")
     parser.add_argument('--summary', action='store_true')
-    parser.add_argument('--dataset', type=str, default='Reuters', choices=['Reuters', 'AAPD', 'IMDB', 'Yelp2014'])
+    parser.add_argument('--dataset', type=str, default='Reuters', choices=['Reuters', 'AAPD', 'IMDB', 'Yelp2014', 'Robust04', 'Robust45', 'Robust05'])
 
     args = parser.parse_args()
     return args
--- lib/data/fetch.py
+++ lib/data/fetch.py
@@ -90,3 +90,71 @@ def imdb():
             label = [int(x) for x in label]
             test_split.append((label, text))
     return train_split, validation_split, test_split
+
+
+def robust04():
+    train_split, validation_split = dict(), dict()
+    topics = ['307', '310', '321', '325', '330', '336', '341', '344', '345', '347', '350', '353', '354', '355', '356',
+              '362', '363', '367', '372', '375', '378', '379', '389', '393', '394', '397', '399', '400', '404', '408',
+              '414', '416', '419', '422', '423', '426', '427', '433', '435', '436', '439', '442', '443', '445', '614',
+              '620', '626', '646', '677', '690']
+
+    for topic in topics:
+        train_split[topic] = list()
+        validation_split[topic] = list()
+        with open(os.path.join('data', 'trec', 'robust04_train_%s.tsv' % topic)) as tsv_file:
+            for line in tsv_file:
+                label, docid, text = line.split('\t')
+                label = 0 if label == '01' else 1
+                train_split[topic].append((label, text))
+        with open(os.path.join('data', 'trec', 'robust04_dev_%s.tsv' % topic)) as tsv_file:
+            for line in tsv_file:
+                label, docid, text = line.split('\t')
+                label = 0 if label == '01' else 1
+                validation_split[topic].append((label, text))
+    return train_split, validation_split, topics
+
+
+def robust45():
+    train_split, validation_split = dict(), dict()
+    topics = ['307', '310', '321', '325', '330', '336', '341', '344', '345', '347', '350', '353', '354', '355', '356',
+              '362', '363', '367', '372', '375', '378', '379', '389', '393', '394', '397', '399', '400', '404', '408',
+              '414', '416', '419', '422', '423', '426', '427', '433', '435', '436', '439', '442', '443', '445', '614',
+              '620', '626', '646', '677', '690']
+
+    for topic in topics:
+        train_split[topic] = list()
+        validation_split[topic] = list()
+        with open(os.path.join('data', 'trec', 'robust45_train_%s.tsv' % topic)) as tsv_file:
+            for line in tsv_file:
+                label, docid, text = line.split('\t')
+                label = 0 if label == '01' else 1
+                train_split[topic].append((label, text))
+        with open(os.path.join('data', 'trec', 'robust45_dev_%s.tsv' % topic)) as tsv_file:
+            for line in tsv_file:
+                label, docid, text = line.split('\t')
+                label = 0 if label == '01' else 1
+                validation_split[topic].append((label, text))
+    return train_split, validation_split, topics
+
+
+def robust05():
+    train_split, validation_split = dict(), dict()
+    topics = ['307', '310', '325', '330', '336', '341', '344', '345', '347', '353', '354', '362', '363', '367', '372',
+              '375', '378', '389', '393', '394', '397', '399', '404', '408', '416', '419', '426', '427', '433', '435',
+              '436', '439', '443']
+
+    for topic in topics:
+        train_split[topic] = list()
+        validation_split[topic] = list()
+        with open(os.path.join('data', 'trec', 'robust05_train_%s.tsv' % topic)) as tsv_file:
+            for line in tsv_file:
+                label, docid, text = line.split('\t')
+                label = 0 if label == '01' else 1
+                train_split[topic].append((label, text))
+        with open(os.path.join('data', 'trec', 'robust05_dev_%s.tsv' % topic)) as tsv_file:
+            for line in tsv_file:
+                label, docid, text = line.split('\t')
+                label = 0 if label == '01' else 1
+                validation_split[topic].append((label, text))
+    return train_split, validation_split, topics
